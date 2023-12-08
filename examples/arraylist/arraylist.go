fc5f66e860235109455676242c9304d8346ebd6b
geange <geange@outlook.com>
2023-12-08 22:42:56 +0800

    feature: 更新golang版本

--- examples/arraylist/arraylist.go
+++ examples/arraylist/arraylist.go
@@ -11,21 +11,25 @@ import (
 
 // ArrayListExample to demonstrate basic usage of ArrayList
 func main() {
+	eq := func(a, b string) bool {
+		return a == b
+	}
+
 	list := arraylist.New[string]()
-	list.Add("a")                         // ["a"]
-	list.Add("c", "b")                    // ["a","c","b"]
-	list.Sort(cmp.Compare[string])        // ["a","b","c"]
-	_, _ = list.Get(0)                    // "a",true
-	_, _ = list.Get(100)                  // nil,false
-	_ = list.Contains("a", "b", "c")      // true
-	_ = list.Contains("a", "b", "c", "d") // false
-	list.Swap(0, 1)                       // ["b","a",c"]
-	list.Remove(2)                        // ["b","a"]
-	list.Remove(1)                        // ["b"]
-	list.Remove(0)                        // []
-	list.Remove(0)                        // [] (ignored)
-	_ = list.Empty()                      // true
-	_ = list.Size()                       // 0
-	list.Add("a")                         // ["a"]
-	list.Clear()                          // []
+	list.Add("a")                             // ["a"]
+	list.Add("c", "b")                        // ["a","c","b"]
+	list.Sort(cmp.Compare[string])            // ["a","b","c"]
+	_, _ = list.Get(0)                        // "a",true
+	_, _ = list.Get(100)                      // nil,false
+	_ = list.Contains(eq, "a", "b", "c")      // true
+	_ = list.Contains(eq, "a", "b", "c", "d") // false
+	list.Swap(0, 1)                           // ["b","a",c"]
+	list.Remove(2)                            // ["b","a"]
+	list.Remove(1)                            // ["b"]
+	list.Remove(0)                            // []
+	list.Remove(0)                            // [] (ignored)
+	_ = list.Empty()                          // true
+	_ = list.Size()                           // 0
+	list.Add("a")                             // ["a"]
+	list.Clear()                              // []
 }
